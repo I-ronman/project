1dca08f974b38c98e99549759889366b2bf48ca7
전대원 <tnlfem2100@naver.com>
2025-07-21 16:58:13 +0900

    training cam이랑 플라스크 cors허용 시도중

--- IronmanFlask/run.py
+++ IronmanFlask/run.py
@@ -1,10 +1,11 @@
 from flask import Flask,jsonify
 from flask_socketio import SocketIO, send
+from flask_cors import CORS
 import base64
 
 app = Flask(__name__)
 socket_io = SocketIO(app)
-
+CORS(app,resources={r'*': {'origins': 'localhost:923'}})
 
 @socket_io.on('analyze')
 def analyze(data):
