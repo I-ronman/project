23c7dbf6685e319e31f8fcc903c2677cf7863bb7
전대원 <tnlfem2100@naver.com>
2025-07-21 15:50:30 +0900

    리액트 포트번호 변경 flask세팅

--- IronmanFlask/run.py
+++ IronmanFlask/run.py
@@ -1,1 +1,18 @@
 from flask import Flask,jsonify
+from flask_socketio import SocketIO, send
+import base64
+
+app = Flask(__name__)
+socket_io = SocketIO(app)
+
+
+@socket_io.on('analyze')
+def analyze(data):
+    image_data = data['image']
+    print(image_data)
+    image_bytes = base64.b64decode(image_data.split(',')[1])
+
+
+
+if __name__ == '__main__':
+    socket_io.run(app, debug=True, port=525)
--- IronmanView/src/components/TrainingCam.jsx
+++ IronmanView/src/components/TrainingCam.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Webcam from "react-webcam";
 
 function TrainingCam() {
+  const ws = useRef<WebSocket | null> (null)
+  useEffect(()=>{
+    ws.current = new WebSocket("localhost:525/analyze")
+    ws.current.onopen = () => {
+      console.log('WebSocket connection opened.')
+      setIsLoading(false);
+    }
+    ws.current.onmessage = (event) => {
+      if (event.data) {
+        const base64ImageData = 'data:image/jpg;base64,' + event.data;
+        setCctvData(base64ImageData);
+      }
+    };
+    ws.current.onerror = () => console.log('WebSocket Error');
+    ws.current.onclose = () => {
+      console.log('Websocket connection is closed');
+    };
+
+    return () => {
+      if (ws.current && ws.current.readyState === 1) {
+        ws.current.close();
+      }
+    };
+  },[]);
   const WebcamComponent = () => <Webcam />;
   return (
     
