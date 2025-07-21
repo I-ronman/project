dbdf3f97051f532a42425ea292cda6c1b41fdf07
전대원 <tnlfem2100@naver.com>
2025-07-21 12:24:30 +0900

    webcam 구현

--- IronmanView/src/App.jsx
+++ IronmanView/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage' 
+import Training from "./components/TrainingCam"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/training" element={<Training />} />
       </Routes>
     </Router>
   )
---
+++ IronmanView/src/components/TrainingCam.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Webcam from "react-webcam";
+
+function TrainingCam() {
+  const WebcamComponent = () => <Webcam />;
+  return (
+    
+    <div>
+      <Webcam/>
+    </div>
+  )
+}
+
+export default TrainingCam
